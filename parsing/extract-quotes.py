254cc3bc1d86ab277ab66e9b30d085d16616ad5e
Keanu Kerr <keanukerr@gmail.com>
2021-08-16 08:20:50 -0500

    Changes to handle multiple source files

--- parsing/extract-quotes.py
+++ parsing/extract-quotes.py
@@ -1,60 +1,76 @@
 #!/usr/bin/python
 
 import sys
+import os
 import json
-import pickle
+import csv
 import re
 
-quotes = {}
+screenplay_path = "../media-scripts/"
+character_path = "../models/characters"
+all_characters_path = "../models/all-characters"
+triggers_path = "../models/triggers"
 
-def extract_quotes(media_name):
-	file = open("media-scripts/" + media_name + "/" + media_name + ".txt")
-	lines  = file.readlines()
-	i = 0
-	while i < len(lines):
-		# Skip lines which do not contain character names
-		is_name = True
-		for c in lines[i].strip():
-			if c.isalpha() and not c.isupper():
-				is_name = False
-		if not is_name:
-			i += 1
-			continue
+def write_all_to_file(media_name, quotes):
+	global all_characters_path
+	with open(all_characters_path + media_name + '.json', 'w', encoding = 'utf8') as f:
+		json.dump(quotes, f, ensure_ascii = False)
 
-		if lines[i].startswith(" ") and lines[i + 1].startswith(" "):
-			name = ''
-			for word in lines[i].strip().split(" "):
-				if word and word[0].isalpha():
-					name += word + " "
-				else:
-					break
-			quote = ''
+def write_to_file(character_name, quotes):
+	global character_path
+	with open(character_path + character_name + '.csv', 'w', encoding = 'utf8') as f:
+		csv.writer.writerow(quotes[character_name]);
 
-			i += 1;
+def extract_quotes(media_name, character_name):
+	global screenplay_path
+	quotes = {}
+	with os.scandir(screenplay_path + media_name) as target:
+		for entry in target:
+			if entry.name.endswith(".txt") and entry.is_file():	
+				file = open(screenplay_path + media_name + "/" + entry.name, "r")
+				lines  = file.readlines()
+				i = 0
+				while i < len(lines):
+					# Skip lines which do not contain character names
+					is_name = True
+					for c in lines[i].strip():
+						if c.isalpha() and not c.isupper():
+							is_name = False
+					if not is_name:
+						i += 1
+						continue
 
-			while not lines[i].strip() or (lines[i].startswith(" ") and not lines[i].split()[0].isupper()):
-				quote = quote + lines[i].strip() + " ";
-				i += 1
+					if lines[i].startswith(" ") and lines[i + 1].startswith(" "):
+						name = ''
+						for word in lines[i].strip().split(" "):
+							if word and word[0].isalpha():
+								name += word + " "
+							else:
+								break
+						quote = ''
 
-			i -= 1
+						i += 1;
 
-			# Removes text within parentheses, replaces multiple spaces with a single space, and removes trailing spaces
-			name = re.sub(r'\([^)]*\)', '', ' '.join(name.strip().split()))
-			quote = re.sub(r'\([^)]*\)', '', ' '.join(quote.strip().split()))
-			if len(name) > 0 and len(quote) > 0:
-				if name in quotes:
-					quotes[name].append(quote)
-				else:
-					quotes[name] = [quote]
-		i += 1
-	file.close()
+						while not lines[i].strip() or (lines[i].startswith(" ") and not lines[i].split()[0].isupper()):
+							quote = quote + lines[i].strip() + " ";
+							i += 1
 
-	with open('../models/' + media_name + '.json', 'w', encoding='utf8') as f:
-		json.dump(quotes, f, ensure_ascii=False)
+						i -= 1
 
-	with open('../models/' + media_name + '.pickle', 'wb') as f:
-		pickle.dump(quotes, f)
-	
+						# Removes text within parentheses, replaces multiple spaces with a single space, and removes trailing spaces
+						name = re.sub(r'\([^)]*\)', '', ' '.join(name.strip().split()))
+						quote = re.sub(r'\([^)]*\)', '', ' '.join(quote.strip().split()))
+						if len(name) > 0 and len(quote) > 0:
+							if name in quotes:
+								quotes[name].append(quote)
+							else:
+								quotes[name] = [quote]
+					i += 1
+				file.close()
+
+	write_all_to_file(media_name, quotes)
+	write_to_file(media_name, quotes)
+
 	print("Success")
 
 extract_quotes(sys.argv[1])
